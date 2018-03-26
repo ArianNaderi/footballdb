6912ba9ea48ee1ca09182b02e5b56a421291d062
Atsushi Yamamoto <yamaatsushi927@gmail.com>
2018-03-26 01:01:35 -0700

    Add login page

---
+++ pages/login/index.vue
@@ -0,0 +1,115 @@
+<template>
+  <section class="user-view">
+  <div class="content">
+    <div class="subsection">
+      <div class="error-msg" v-if="this.$store.state.errorMsg">
+        <p>{{ this.$store.state.errorMsg }}</p>
+      </div>
+      <form style="margin: 15px 15px;">
+        <div style="margin: 10px 0;">
+          <span class="user-username">Username: </span>
+          <input type="text" :value="username" v-model="username"></input>
+        </div>
+        <div style="margin: 10px 0;">
+          <span class="user-password">Password: </span>
+          <input type="password" v-model="password"></input>
+        </div>
+      </form>
+      <button type="button" class="button--grey" @click="submitLogin">Login</button>
+    </div>
+  </div>
+  </section>
+</template>
+
+<script>
+import axios from '~/plugins/axios'
+
+export default {
+
+  data () {
+    return {
+      userid: '',
+      username: '',
+      password: '',
+      permission: '',
+    }
+  },
+
+  methods: {
+    submitLogin () {
+      let self = this
+
+      axios.post('/login', {
+        headers:
+          {
+            'Content-Type': 'application/json'
+          },
+        data:
+          {
+            userid: self.userid,
+            username: self.username,
+            password: self.password,
+            permission: self.permission
+          }})
+        .then((res) => {
+          self.$store.commit('login', {
+            username: res.data.user.username,
+            permission: res.data.user.permission
+          })
+          self.$store.commit('noError')
+          self.$nuxt.$router.replace({ path: '/' })
+        })
+        .catch((e) => {
+          self.$store.commit('logout')
+          self.$store.commit('newError', 'Login Failed')
+          console.log("Login Failed")
+          console.log(e)
+        })
+    }
+  },
+
+  head () {
+    return {
+      title: `Login`
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.user-view
+  padding-top 0
+
+.content
+  position absolute
+  width 100%
+
+.subsection
+  background-color #fff
+  border-radius 2px
+  margin 25px 0
+  transition all .5s cubic-bezier(.55,0,.1,1)
+  padding 10px 30px 10px 30px
+  position relative
+  line-height 20px
+  .error-msg
+    color: red
+    margin-left: 15px
+  .subsection-title
+    margin 25px 10px
+    font-size 26px
+    font-weight 500
+  .user-username
+    font-size 24px
+    font-weight 500
+    color #707070
+  .user-password
+    font-size 24px
+    font-weight 500
+    color #707070
+  a
+    text-decoration underline
+  &:hover
+    color #515ec4
+
+</style>
--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
 import express from 'express'
 import { Nuxt, Builder } from 'nuxt'
 
+// account imports
+import passport from 'passport'
+import Sequelize from 'sequelize'
+import connection from './configs/sequelize'
+const LocalStrategy = require('passport-local').Strategy
+import session from 'express-session'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import flash from 'connect-flash'
+
 import api from './api'
 
 const app = express()
@@ -11,6 +21,72 @@ app.set('port', port)
 
 // Import API Routes
 app.use('/api', api)
+
+// ============================
+// Account log in/out
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(session({secret: 'our secret string'}));
+app.use(cookieParser());
+app.use(passport.initialize()); // <-- Register the Passport middleware.
+
+const User = connection.define('users', {
+  userid: {
+    type: Sequelize.INTEGER,
+    primaryKey: true
+  },
+  username: Sequelize.STRING(32),
+  password: Sequelize.TEXT
+})
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.use(new LocalStrategy((username, password, done) => {
+  User.findOne({
+      where: {username: username, password: password},
+      attributes: ['userid', 'username', 'password', 'permission']
+    })
+    .then(user => {
+      if (user !== null) {
+        return done(null, user)
+      } else {
+        return done('User not found')
+      }
+    })
+}))
+
+app.post('/login',
+  (req,res,next) => {
+    // Ex passport obj { user: { userid: 5, username: 'atsushi', password: 'cs304' } }
+    if (req.session && req.session.passport) {
+      console.log('Passport: user is logged in: ', req.session.passport);
+    } else {
+      console.log('Passport: user not logged in');
+    }
+
+    // Required for local passport to initiate
+    req.body.username = req.body.data.username
+    req.body.password = req.body.data.password
+    passport.authenticate('local', {
+      failureRedirect: '/login',
+    })(req,res,next)
+  },
+  // This is called if login is successful
+  function (req, res) {
+    console.log("body parsing", req.body);
+    res.status(200).send(req.session.passport)
+  }
+)
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/'); //Can fire before session is destroyed?
+});
+
+// ============================
 
 // Import and Set Nuxt.js options
 let config = require('../nuxt.config.js')
